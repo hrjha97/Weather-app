ad0aab3b6e5a3197b62eb08142844a6cbf0e0e6a
Harsh Ranjan Jha <jha.harsh837@gmail.com>
2024-02-09 13:35:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static( "public"));
+app.use(express.static(__dirname + "/public/"));
 
 
 app.get("/", function(req, res){
